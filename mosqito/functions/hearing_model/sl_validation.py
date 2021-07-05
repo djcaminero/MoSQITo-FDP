5b1091cc50d3012ab7def783d5793be6ff682a1d
djcaminero <70754385+djcaminero@users.noreply.github.com>
2021-07-05 02:05:03 +0200

    Update sl_validation.py

--- mosqito/functions/hearing_model/sl_validation.py
+++ mosqito/functions/hearing_model/sl_validation.py
@@ -46,14 +46,14 @@ def out_mid_validation(fs, signal, signal_filtered, sos_ear):
     plt.semilogx(w_om_hertz, h_om_db)
     # Legend created following the guidelines from:
     #   https://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
-    plt.legend(['F. Response (sos filter)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+    plt.legend(['Curva del filtro auditivo'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
     plt.subplots_adjust(right=0.75)
 
     plt.xlim(20, 20000)
     plt.ylim(-25, 10)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dB]')
-    plt.title('Frequency Response of Outer and Middle Ear Filter')
+    plt.xlabel('Frecuencia [Hz]')
+    plt.ylabel('Nivel de presión sonora [dB SPL]')
+    plt.title('Respuesta en frecuencia del oído externo y medio/interno')
     plt.grid(which='both', linestyle='-', color='grey')
     plt.xticks([20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000],
                ["20", "50", "100", "200", "500", "1K", "2K", "5K", "10K", "20K"])
@@ -62,44 +62,44 @@ def out_mid_validation(fs, signal, signal_filtered, sos_ear):
     if dim == 2:
         # Left channel
         plt.figure(figsize=(10, 5))
-        plt.plot(time, signal_filtered[:, 0], 'r', label='Filtered signal after OMF (L channel)')
-        plt.plot(time, signal[:, 0], 'b', label='Input signal (L channel)')
+        plt.plot(time, signal_filtered[:, 0], 'r', label='Señal filtrada por el oído (Canal 1)')
+        plt.plot(time, signal[:, 0], 'b', label='Señal de entrada (Canal 1)')
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
-        plt.subplots_adjust(right=0.68)
+        plt.subplots_adjust(right=0.7)
 
         plt.xlim(0, len(signal_filtered))
-        plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude [Pa]')
-        plt.title('Comparison between filtered signal and input signal (Left channel)')
+        plt.xlabel('Tiempo [s]')
+        plt.ylabel('Amplitud [Pa]')
+        plt.title('Filtrado del oído externo y medio/interno (Canal 1)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
         # Right channel
         plt.figure(figsize=(10, 5))
-        plt.plot(time, signal_filtered[:, 1], 'r', label='Filtered signal after OMF (R channel)')
-        plt.plot(time, signal[:, 1], 'b', label='Input signal (R channel)')
+        plt.plot(time, signal_filtered[:, 1], 'r', label='Señal filtrada por el oído (Canal 2)')
+        plt.plot(time, signal[:, 1], 'b', label='Señal de entrada (Canal 2)')
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
-        plt.subplots_adjust(right=0.68)
+        plt.subplots_adjust(right=0.7)
 
         plt.xlim(0, len(signal_filtered))
         plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude [Pa]')
-        plt.title('Comparison between filtered signal and input signal (Right channel)')
+        plt.ylabel('Amplitud [Pa]')
+        plt.title('Filtrado del oído externo y medio/interno (Canal 2)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
     elif dim == 1:
         # Mono channel
         plt.figure(figsize=(10, 5))
-        plt.plot(time, signal_filtered, 'r', label='Filtered signal after OMF (Mono)')
-        plt.plot(time, signal, 'b', label='Input signal (Mono)')
+        plt.plot(time, signal_filtered, 'r', label='Señal filtrada por el oído')
+        plt.plot(time, signal, 'b', label='Señal de entrada')
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
-        plt.subplots_adjust(right=0.68)
+        plt.subplots_adjust(right=0.7)
 
         plt.xlim(0, x_limit)
         plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude [Pa]')
-        plt.title('Comparison between filtered signal and input signal (Mono channel)')
+        plt.ylabel('Amplitud [Pa]')
+        plt.title('Filtrado del oído externo y medio/interno')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
@@ -152,15 +152,15 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
     time = np.arange(len(band_pass_signal_array[0])) / fs
     x_limit = len(band_pass_signal_array[0]) / fs
 
-    print('\n\n_____________AUDITORY FILTERING BANK_____________\n')
-    print('Central Frequencies:\n' + str(centre_freq_array) + '\n')
-    print('Filter Bandwidths:\n' + str(f_bandwidth_array) + '\n')
-    print('T. Delay:\n' + str(t_delay_array) + '\n')
-    print('D coefficients:\n' + str(d_coefficients_array) + '\n')
-    print('Sb values - block size:\n' + str(sb_array) + '\n')
-    print('Sh values - hop size:\n' + str(sh_array) + '\n')
-    print('Am coefficients - band-pass filter:\n' + str(am_mod_coefficient_array) + '\n')
-    print('Bm coefficients - band-pass filter:\n' + str(bm_mod_coefficient_array) + '\n')
+    print('\n\n_____________Banco de Filtros Auditivos_____________\n')
+    print('Frecuencias de corte:\n' + str(centre_freq_array) + '\n')
+    print('Anchos de banda:\n' + str(f_bandwidth_array) + '\n')
+    print('Retardos:\n' + str(t_delay_array) + '\n')
+    print('Coeficientes d:\n' + str(d_coefficients_array) + '\n')
+    print('Sb - Tamaño de bloque:\n' + str(sb_array) + '\n')
+    print('Sh - Distancia entre bloques:\n' + str(sh_array) + '\n')
+    print('Coeficientes am - filtro paso-banda:\n' + str(am_mod_coefficient_array) + '\n')
+    print('Coeficientes bm - filtro paso-banda:\n' + str(bm_mod_coefficient_array) + '\n')
 
     """ GRAPHIC REPRESENTATION """
     # Frequency response of the filters
@@ -172,17 +172,17 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         w_afb_hertz = w_afb * (fs / (2.0 * math.pi))
         h_afb_db = 20.0 * np.log10(np.abs(h_afb))
         plt.semilogx(w_afb_hertz, h_afb_db,
-                     label='FR band: ' + str(band_number)
-                           + ' (' + str(round(float(centre_freq_array[afb_band]), 2)) + 'Hz filter)')
+                     label='RF banda: ' + str(band_number)
+                           + ' (' + str(round(float(centre_freq_array[afb_band]), 2)) + 'Hz)')
 
     plt.legend(bbox_to_anchor=(1.04, 1), borderaxespad=0, loc='upper left', ncol=2)
     plt.subplots_adjust(right=0.65)
 
     plt.xlim(right=20000)
     plt.ylim(bottom=-250)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dB]')
-    plt.title('Auditory Filtering Bank')
+    plt.xlabel('Frecuencia [Hz]')
+    plt.ylabel('Nivel de presión sonora [dB SPL]')
+    plt.title('Banco de filtros auditivos')
     plt.grid(which='both', linestyle='-', color='grey')
     plt.xticks([20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000],
                ["20", "50", "100", "200", "500", "1K", "2K", "5K", "10K", "20K"])
@@ -192,32 +192,32 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         # Left channel
         plt.figure(figsize=(10, 5))
         [plt.plot(time, band_pass_signal_array[band_num, :, 0],
-                  label='Band-pass signal ' + str(band_num + 1) + ' - '
+                  label='Señal paso-banda ' + str(band_num + 1) + ' - '
                         + str(round(float(centre_freq_array[band_num]), 2)) + 'Hz)') for band_num in rep_bands]
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.65)
 
         plt.xlim(0, x_limit)
         plt.ylim(bottom=0)
-        plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude [Pa]')
-        plt.title('Band-pass signals after filtering with AFB (Left channel)')
+        plt.xlabel('Tiempo [s]')
+        plt.ylabel('Amplitud [Pa]')
+        plt.title('Señales paso-banda después del filtrado con el BFA (Canal 1)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
         # Right channel
         plt.figure(figsize=(10, 5))
         [plt.plot(time, band_pass_signal_array[band_num, :, 1],
-                  label='Band-pass signal ' + str(band_num + 1) + ' - '
+                  label='Señal paso-banda ' + str(band_num + 1) + ' - '
                         + str(round(float(centre_freq_array[band_num]), 2)) + 'Hz)') for band_num in rep_bands]
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.65)
 
         plt.xlim(0, x_limit)
         plt.ylim(bottom=0)
-        plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude [Pa]')
-        plt.title('Band-pass signals after filtering with AFB (Right channel)')
+        plt.xlabel('Tiempo [s]')
+        plt.ylabel('Amplitud [Pa]')
+        plt.title('Señales paso-banda después del filtrado con el BFA (Canal 2)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
@@ -225,16 +225,16 @@ def afb_validation(fs, dim, centre_freq_array, f_bandwidth_array, t_delay_array,
         # Mono channel
         plt.figure(figsize=(10, 5))
         [plt.plot(time, band_pass_signal_array[band_num],
-                  label='Band-pass signal ' + str(band_num + 1) + ' - '
+                  label='Señal paso-banda ' + str(band_num + 1) + ' - '
                         + str(round(float(centre_freq_array[band_num]), 2)) + 'Hz)') for band_num in rep_bands]
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.65)
 
         plt.xlim(0, x_limit)
         plt.ylim(bottom=0)
-        plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude [Pa]')
-        plt.title('Band-pass signals after filtering with AFB (Mono channel)')
+        plt.xlabel('Tiempo [s]')
+        plt.ylabel('Amplitud [Pa]')
+        plt.title('Señales paso-banda después del filtrado con el BFA')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
@@ -262,46 +262,48 @@ def sl_validation(dim, centre_freq_array, n_array):
     rep_blocks = np.array([str(round(n_blocks / 12)), str(round(n_blocks / 8)), str(round(n_blocks / 4)),
                            str(round(n_blocks / 2)), str(int(float((n_blocks * 3) / 4)))])
 
+    rep_blocks = np.array([17])
+
     """ EXTRACTION OF PARAMETERS & GRAPHIC REPRESENTATION """
     # If n_array has 2 dimensions (stereo), we have to extract the data dimension by dimension
     if dim == 2:
         # Left channel
-        print('Specific loudness values (Left channel, stereo signal):\n' + str(n_array[:, :, 0]) + '\n')
-        np.savetxt("specific_loudness_left.txt", n_array[:, :, 0], fmt='%f',
+        print('Valores de la sonoridad específica (Canal 1):\n' + str(n_array[:, :, 0]) + '\n')
+        np.savetxt("sonoridad_específica_CANAL1.txt", n_array[:, :, 0], fmt='%f',
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
         [plt.semilogx(centre_freq_array, n_array[int(block_num), :, 0],
-                      label='SL values, block: ' + str(int(block_num + 1))) for block_num in rep_blocks]
+                      label='Valores SE, bloque: ' + str(int(block_num + 1))) for block_num in rep_blocks]
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=50, right=15000)
         plt.ylim(bottom=0)
-        plt.xlabel('Frequency [Hz]')
-        plt.ylabel('Sound pressure [sones]')
-        plt.title('Specific loudness contours (Left channel), block nº: ')
+        plt.xlabel('Frecuencia [Hz]')
+        plt.ylabel('Nivel de presión sonora [soniosHMS]')
+        plt.title('Sonoridad específica (Canal 1), banda de 1 kHz')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.xticks([50, 100, 200, 500, 1000, 2000, 5000, 10000, 15000],
                    ["50", "100", "200", "500", "1K", "2K", "5K", "10K", "15K"])
         plt.show()
 
         # Right channel
-        print('Specific loudness values (Right channel, stereo signal):\n' + str(n_array[:, :, 1]) + '\n')
-        np.savetxt("specific_loudness_right.txt", n_array[:, :, 1], fmt='%f',
+        print('Valores de la sonoridad específica (Canal 2):\n' + str(n_array[:, :, 1]) + '\n')
+        np.savetxt("sonoridad_específica_CANAL2.txt", n_array[:, :, 1], fmt='%f',
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
         [plt.semilogx(centre_freq_array, n_array[int(block_num), :, 1],
-                      label='SL values, block: ' + str(int(block_num + 1))) for block_num in rep_blocks]
+                      label='Valores SE, bloque: ' + str(int(block_num + 1))) for block_num in rep_blocks]
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=50, right=15000)
         plt.ylim(bottom=0)
-        plt.xlabel('Frequency [Hz]')
-        plt.ylabel('Sound pressure [sones]')
-        plt.title('Specific loudness contours (Right channel), block nº: ')
+        plt.xlabel('Frecuencia [Hz]')
+        plt.ylabel('Nivel de presión sonora [soniosHMS]')
+        plt.title('Sonoridad específica (Canal 2), banda de 1 kHz')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.xticks([50, 100, 200, 500, 1000, 2000, 5000, 10000, 15000],
                    ["50", "100", "200", "500", "1K", "2K", "5K", "10K", "15K"])
@@ -309,21 +311,21 @@ def sl_validation(dim, centre_freq_array, n_array):
 
     elif dim == 1:
         # Mono channel
-        print('Specific loudness values (Mono signal):\n' + str(n_array) + '\n')
-        np.savetxt("specific_loudness.txt", n_array[:, :, 0], fmt='%f',
+        print('Valores de la sonoridad específica:\n' + str(n_array) + '\n')
+        np.savetxt("sonoridad_específica.txt", n_array[:, :, 0], fmt='%f',
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
         [plt.semilogx(centre_freq_array, n_array[int(block_num), :, 0],
-                      label='SL values, block: ' + str(int(block_num))) for block_num in rep_blocks]
+                      label='Valores SE, bloque: ' + str(int(block_num))) for block_num in rep_blocks]
         plt.legend(bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=50, right=15000)
         plt.ylim(bottom=0)
-        plt.xlabel('Frequency [Hz]')
-        plt.ylabel('Sound pressure [sones]')
-        plt.title('Specific loudness contours (Mono channel), block nº: ')
+        plt.xlabel('Frecuencia [Hz]')
+        plt.ylabel('Nivel de presión sonora [soniosHMS]')
+        plt.title('Sonoridad específica, ISO_532-1, 1 kHz 60 dB')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.xticks([50, 100, 200, 500, 1000, 2000, 5000, 10000, 15000],
                    ["50", "100", "200", "500", "1K", "2K", "5K", "10K", "15K"])
@@ -345,50 +347,50 @@ def tl_validation(dim, t_array):
     -------
 
     """
-    print('Total loudness values:\n' + str(t_array) + '\n')
+    print('Valores de la sonoridad total:\n' + str(t_array) + '\n')
 
     """ EXTRACTION OF PARAMETERS & GRAPHIC REPRESENTATION """
     # If n_array has 2 dimensions (stereo), we have to extract the data dimension by dimension
     if dim == 2:
         # Left channel
-        print('Total loudness values (Left channel, stereo signal):\n' + str(t_array[:, 0]) + '\n')
-        np.savetxt("total_loudness_left.txt", t_array[:, 0], fmt='%f',
+        print('Valores de la sonoridad total (CANAL 1):\n' + str(t_array[:, 0]) + '\n')
+        np.savetxt("sonoridad_total_CANAL1.txt", t_array[:, 0], fmt='%f',
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
         plt.plot(t_array[:, 0])
-        plt.legend(['Total loudness (signal)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.legend(['Sonoridad total de la señal'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=0, right=len(t_array))
         plt.ylim(bottom=0)
-        plt.xlabel('Block number')
-        plt.ylabel('Sound pressure [sones]')
-        plt.title('Total loudness over time (Left channel)')
+        plt.xlabel('Número de bloque')
+        plt.ylabel('Nivel de presión sonora [soniosHMS]')
+        plt.title('Valor de la sonoridad total por número de bloque (CANAL 1)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
         # Right channel
-        print('Total loudness values (Right channel, stereo signal):\n' + str(t_array[:, 0]) + '\n')
-        np.savetxt("total_loudness_right.txt", t_array[:, 1], fmt='%f',
+        print('Valores de la sonoridad total (CANAL 2):\n' + str(t_array[:, 0]) + '\n')
+        np.savetxt("sonoridad_total_CANAL2.txt", t_array[:, 1], fmt='%f',
                    delimiter='\t')
 
         plt.figure(figsize=(10, 5))
         plt.plot(t_array[:, 0])
-        plt.legend(['Total loudness (signal)'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
+        plt.legend(['Sonoridad total de la señal'], bbox_to_anchor=(1.04, 0.5), borderaxespad=0, loc='center left', ncol=1)
         plt.subplots_adjust(right=0.75)
 
         plt.xlim(left=0, right=len(t_array))
         plt.ylim(bottom=0)
-        plt.xlabel('Block number')
-        plt.ylabel('Sound pressure [sones]')
-        plt.title('Total loudness over time (Right channel)')
+        plt.xlabel('Número de bloque')
+        plt.ylabel('Nivel de presión sonora [soniosHMS]')
+        plt.title('Valor de la sonoridad total por número de bloque (CANAL 2)')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
     elif dim == 1:
-        print('Total loudness values (Mono signal):\n' + str(t_array) + '\n')
-        np.savetxt("total_loudness.txt", t_array, fmt='%f', delimiter='\t')
+        print('Valores de la sonoridad total:\n' + str(t_array) + '\n')
+        np.savetxt("sonoridad_total.txt", t_array, fmt='%f', delimiter='\t')
 
         plt.figure(figsize=(10, 5))
         plt.plot(t_array[:, 0])
@@ -397,9 +399,9 @@ def tl_validation(dim, t_array):
 
         plt.xlim(left=0, right=len(t_array))
         plt.ylim(bottom=0)
-        plt.xlabel('Block number')
-        plt.ylabel('Sound pressure [sones]')
-        plt.title('Total loudness over time (Mono channel)')
+        plt.xlabel('Número de bloque')
+        plt.ylabel('Nivel de presión sonora [soniosHMS]')
+        plt.title('Valor de la sonoridad total por número de bloque, ISO_532-1, 1 kHz 60 dB')
         plt.grid(which='both', linestyle='-', color='grey')
         plt.show()
 
